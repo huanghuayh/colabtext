7932d1de9ce94e03a8b17af4d7be726a9cdc3a40
RAHUL S H <rahulhoskeri7@gmail.com>
2025-11-05 14:26:00 -0800

    exp 2
    1) conv fuse the harmonic down sampled than just summation

--- models/ppsp_1up.py
+++ models/ppsp_1up.py
@@ -63,9 +63,9 @@ class PPSP_1up(nn.Module):
         super(PPSP_1up, self).__init__()
 
         self.ppsp_backbone = ppsp_backbone
-
+        self.scale_fuse = nn.Conv1d(3, 1, 1)
         self.fund_head = nn.Sequential(
-            nn.Linear(1365, hidden_nodes),
+            nn.Linear(1024, hidden_nodes),
             nn.ReLU(),
             nn.Dropout(0.3),
             nn.Linear(hidden_nodes, 1024)
@@ -78,17 +78,20 @@ class PPSP_1up(nn.Module):
         remade_harmonics_pred = []
         for ind in range(harmonics_pred.shape[0]):
             cur_scaled_lst=[]
-            for scale_factor in [1,2,3]:
-                cur_scaled = scale_psd(harmonics_pred[ind],int(len(harmonics_pred[ind])//scale_factor),scale_factor,method="average")
+            for sf in [1, 2, 3]:
+                cur_scaled = scale_psd(harmonics_pred[ind],
+                                int(len(harmonics_pred[ind]) // sf),
+                                sf, method="average")
+                # cur_scaled = torch.from_numpy(cur_scaled).float().to(x.device)
+                cur_scaled = torch.nn.functional.interpolate(cur_scaled.unsqueeze(0).unsqueeze(0),
+                                                      size=harmonics_pred.shape[-1],
+                                                      mode="linear", align_corners=False).squeeze()
                 cur_scaled_lst.append(cur_scaled)
 
             # flatten = torch.cat(cur_scaled_lst)
-            ## summation
-            min_len = min(t.numel() for t in cur_scaled_lst)
-            summed = sum(t[:min_len] for t in cur_scaled_lst)
-            flatten = torch.cat([harmonics_pred[ind], summed], dim=0)
-
-            remade_harmonics_pred.append(flatten)
+            stacked = torch.stack(cur_scaled_lst, dim=0).unsqueeze(0)
+            fused = self.scale_fuse(stacked).squeeze()
+            remade_harmonics_pred.append(fused)
         remade_harmonics_pred = torch.stack(remade_harmonics_pred)
 
         fundamental_pred = self.fund_head(remade_harmonics_pred).unsqueeze(1)
@@ -96,11 +99,11 @@ class PPSP_1up(nn.Module):
         return fundamental_pred, harmonics_pred
 
 
-# import fpn_2
-# import torch
-#
-# x= torch.randn(10, 1, 1024)
-# PPSP_net = fpn_2.PPSP(in_channels=1, out_channels=32)
-# model_ppsp_1up = PPSP_1up(PPSP_net, hidden_nodes=256)
-# fund_pred,harmonic_pred =model_ppsp_1up(x)
-# print()
+import fpn_2
+import torch
+
+x= torch.randn(10, 1, 1024)
+PPSP_net = fpn_2.PPSP(in_channels=1, out_channels=32)
+model_ppsp_1up = PPSP_1up(PPSP_net, hidden_nodes=256)
+fund_pred,harmonic_pred =model_ppsp_1up(x)
+print()
